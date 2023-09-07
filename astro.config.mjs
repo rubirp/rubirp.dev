5a210dce69e64d9efb01468c6a38edd27a0f61da
rubirp <rubi_almansa@hotmail.com>
2023-09-07 17:53:43 +0200

    Test Sidebar and baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rubirp.github.io',
-    base: '/rubirp.dev',
+  site: 'https://rubirp.github.io',
+  base: '/rubirp.dev',
+  integrations: [tailwind()]
 });
