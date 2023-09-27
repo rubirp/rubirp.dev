d455e450d93d6bc984ac025e66ec452396d85703
rubirp <rubi_almansa@hotmail.com>
2023-09-27 12:10:54 +0200

    output from 'hybrid' to 'static'

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
   site: 'https://rubirp.github.io',
   base: '/rubirp.dev',
   integrations: [tailwind()]
