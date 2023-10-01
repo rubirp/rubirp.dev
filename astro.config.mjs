80ebfc6f7f20a245c57a4887ed3f82252eca1d8d
rubirp <rubi_almansa@hotmail.com>
2023-10-02 00:03:17 +0200

    removed base for config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
 import tailwind from '@astrojs/tailwind';
 
+//  base: '/rubirp.dev',
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rubirp.dev',
-  base: '/rubirp.dev',
   integrations: [tailwind(), astroI18next()]
 });
