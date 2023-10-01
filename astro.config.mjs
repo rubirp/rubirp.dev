def5c834dac486cdabeb86e829e90420acd76497
rubirp <rubi_almansa@hotmail.com>
2023-10-02 00:00:26 +0200

    first deploy 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from 'astro-i18next';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rubirp.github.io',
+  site: 'https://rubirp.dev',
   base: '/rubirp.dev',
   integrations: [tailwind(), astroI18next()]
 });
